bd716be476db8f5a3846d98de22a78806708c4c1
ErinWiggins <32422052+ErinWiggins@users.noreply.github.com>
2017-10-07 14:45:15 -0400

    Code added to main.cpp to print WeakSlingshot stats

--- main.cpp
+++ main.cpp
@@ -47,6 +47,10 @@ int main(int argc, char** argv) {
 	simulateWeapon(weapon, armor);
 	delete(weapon);
 
+	weapon = WeaponFactory::getInstance()->getWeapon("slingshot");
+	simulateWeapon(weapon, armor);
+	delete(weapon);
+
     return 0;
 }
 
