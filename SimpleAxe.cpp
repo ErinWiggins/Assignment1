de8d6781716cd5aec7109e8f7d0c33f71814d36b
ErinWiggins <32422052+ErinWiggins@users.noreply.github.com>
2017-10-07 13:48:08 -0400

    SimpleAxe::hit() completed

--- SimpleAxe.cpp
+++ SimpleAxe.cpp
@@ -7,7 +7,17 @@
 #include "SimpleAxe.h"
 
 double SimpleAxe::hit(double armor) {
+	double damage;
 
+	if (armor > 0 && armor < 20)
+		damage = hitPoints;
+	else
+		damage = hitPoints - armor;
+
+	if (damage < 0)
+		return 0;
+
+	return damage;
 
 	return 0;
 }
